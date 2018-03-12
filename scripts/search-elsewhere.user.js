1af2bd4247541474e93e752c1b5bb98c8c996cf8
Björn Richter <x3ro1989@gmail.com>
2018-03-12 13:33:11 +0100

    Add new userscript "Search Elsewhere"

---
+++ scripts/search-elsewhere.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name Search Elsewhere
+// @author ^x3ro
+// @namespace https://x3ro.net/
+// @version 0.0
+//
+// @description Search current query with other search engines.
+// @homepage https://github.com/x3rAx/userscripts
+// @supportURL https://github.com/x3rAx/userscripts/issues
+// @downloadURL https://rawgit.com/x3rAx/userscripts/master/scripts/search-elsewhere.user.js
+//
+// @include /^https://www.google.[a-z]+/search\?.*?q=.*/
+// @include /^https://duckduckgo.com/\?.*?q=.*/
+// @include /^https://www.youtube.com/.*/
+// @grant none
+// ==/UserScript==
+
